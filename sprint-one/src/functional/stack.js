f063f28c0b9fc5939a7c8abfb2e3b737eed10436
Henry Ng <henryfc.ng@gmail.com>
2015-03-25 19:53:25 -0400

    Progressing with Functional Pattern Stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,4 +1,5 @@
 var Stack = function(){
+  var size = 0;
   var someInstance = {};
 
   // Use an object with numeric keys to store values
@@ -6,12 +7,14 @@ var Stack = function(){
 
   // Implement the methods below
   someInstance.push = function(value){
+    size += 1;
   };
 
   someInstance.pop = function(){
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
