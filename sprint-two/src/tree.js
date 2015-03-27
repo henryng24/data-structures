6987a83e5d646cdf0edb8eda492458f644d84e69
Henry Ng <henryfc.ng@gmail.com>
2015-03-27 15:51:12 -0400

    Trees are Working!

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
+  _.extend(newTree, treeMethods);
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -15,11 +16,24 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var newTree = Tree(value);
+  this.children.push(newTree);
 };
 
 treeMethods.contains = function(target){
-
+  var found = false;
+  var recursiveContains = function(tree, target) {
+    if(tree.value === target) {
+      found = true;
+    }
+    if(tree.children.length > 0) {
+      for(var i = 0; i < tree.children.length; i++) {
+        recursiveContains(tree.children[i], target);
+      }
+    }
+  };
+  recursiveContains(this, target);
+  return found;
 };
 
 
