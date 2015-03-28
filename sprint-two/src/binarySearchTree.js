afc6657c482a5b4b7ed758523f15497cc240a270
Henry Ng <henryfc.ng@gmail.com>
2015-03-28 19:31:46 -0400

    Binary Tree Cleanup!

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -5,39 +5,68 @@ var BinarySearchTree = function(value){
 };
 
 BinarySearchTree.prototype.insert = function(value) {
-  var checkTree = function(value, tree) {
-    if(value < tree.value) {
-      if(tree.left === null) {
-        tree.left = new BinarySearchTree(value);
-      } else {
-        checkTree(value, tree.left)
-      }
-    } else if (value > tree.value) {
-      if(tree.right === null) {
-        tree.right = new BinarySearchTree(value);
-      } else {
-        checkTree(value, tree.right);
-      }
+  if (value < this.value) {
+    if (this.left === null) {
+      this.left = new BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
     }
-  };
-  checkTree(value, this);
+  }
+  if (value > this.value) {
+    if (this.right === null) {
+      this.right = new BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+  
+  // var checkTree = function(value, tree) {
+  //   if(value < tree.value) {
+  //     if(tree.left === null) {
+  //       tree.left = new BinarySearchTree(value);
+  //     } else {
+  //       checkTree(value, tree.left)
+  //     }
+  //   } else if (value > tree.value) {
+  //     if(tree.right === null) {
+  //       tree.right = new BinarySearchTree(value);
+  //     } else {
+  //       checkTree(value, tree.right);
+  //     }
+  //   }
+  // };
+  // checkTree(value, this);
 };
 
 BinarySearchTree.prototype.contains = function(target) {
-  var found = false;
-  var recurse = function(target, tree) {
-    if (target === tree.value) {
-      found = true;
-    }
-    if (target < tree.value && tree.left !== null) {
-      recurse(target, tree.left);
-    }
-    if (target > tree.value && tree.right !== null) {
-      recurse(target, tree.right);
-    }
-  };
-  recurse(target, this);
-  return found;
+  if (target === this.value) {
+    return true;
+  }
+
+  if (target < this.value && this.left !== null) {
+    return this.left.contains(target);
+  }
+
+  if (target > this.value && this.right !== null) {
+    return this.right.contains(target);
+  }
+
+  return false;
+
+  // var found = false;
+  // var recurse = function(target, tree) {
+  //   if (target === tree.value) {
+  //     found = true;
+  //   }
+  //   if (target < tree.value && tree.left !== null) {
+  //     recurse(target, tree.left);
+  //   }
+  //   if (target > tree.value && tree.right !== null) {
+  //     recurse(target, tree.right);
+  //   }
+  // };
+  // recurse(target, this);
+  // return found;
 };
 
 BinarySearchTree.prototype.depthFirstLog = function(callback) {
